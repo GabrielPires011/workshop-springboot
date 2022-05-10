713dee9e47218f07a525be8c6062139eb8d15ee7
Gabriel Teixeira <gabrielpires011@hotmail.com>
2022-05-10 16:40:32 -0300

    Query with multiple criteria

--- src/main/java/com/workshop/repository/PostRepository.java
+++ src/main/java/com/workshop/repository/PostRepository.java
@@ -1,13 +1,21 @@
 package com.workshop.repository;
 
 import com.workshop.domain.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, Integer> {
 
-    List<Post> findByTitleContaining(String text);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
